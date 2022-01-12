7ebd770d88eaee633371f60289d932054497aaa6
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-12 16:22:41 +0100

    implement GET /api/users

--- server.js
+++ server.js
@@ -36,40 +36,57 @@ let ExerciseUser = mongoose.model("ExerciseUser", exerciseUserSchema);
  *                     API routes                            *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/**
- * POST /api/users
- *
- * Creates a new user
- * Request body (URL encoded):
- * - username (String)
- */
-app.post("/api/users", bodyParserUrlEncoded, (req, res) => {
-  const { username } = req.body;
+app
+  .route("/api/users")
+  /**
+   * GET /api/users
+   * 
+   * Returns a list of all users
+   */
+  .get((req, res) => {
+    ExerciseUser.find({}, (err, docs) => {
+      if (err) {
+        console.error(err);
+        res.json({ error: err });
+      } else {
+        res.json(docs);
+      }
+    })
+  })
+  /**
+   * POST /api/users
+   *
+   * Creates a new user
+   * Request body (URL encoded):
+   * - username (String)
+   */
+  .post(bodyParserUrlEncoded, (req, res) => {
+    const { username } = req.body;
 
-  // look up if a user with this name already exists ... if so res error
-  let promise = ExerciseUser.findOne({ username: username }).exec();
-  assert.ok(promise instanceof Promise);
-  promise.then((userObject) => {
-    if (userObject !== null) {
-      // The user exists, so return the object
-      res.json({
-        username: userObject.username,
-        _id: userObject._id,
-      });
-    } else {
-      // The user does not yet exist, so create a new one
-      const newUser = new ExerciseUser({ username: username });
-      newUser.save((err) => {
-        if (err) res.json({ Error: err });
-        else
-          res.json({
-            username: newUser.username,
-            _id: newUser._id,
-          });
-      });
-    }
+    // look up if a user with this name already exists ... if so res error
+    let promise = ExerciseUser.findOne({ username: username }).exec();
+    assert.ok(promise instanceof Promise);
+    promise.then((userObject) => {
+      if (userObject !== null) {
+        // The user exists, so return the object
+        res.json({
+          username: userObject.username,
+          _id: userObject._id,
+        });
+      } else {
+        // The user does not yet exist, so create a new one
+        const newUser = new ExerciseUser({ username: username });
+        newUser.save((err) => {
+          if (err) res.json({ Error: err });
+          else
+            res.json({
+              username: newUser.username,
+              _id: newUser._id,
+            });
+        });
+      }
+    });
   });
-});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
