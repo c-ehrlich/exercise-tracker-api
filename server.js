a7160aedc3fc0cf4b9f066f8217b5060be3cbecc
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-12 16:13:06 +0100

    implement POST /api/users

--- server.js
+++ server.js
@@ -1,18 +1,76 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+const bodyParserUrlEncoded = bodyParser.urlencoded({ extended: false });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                        Setup DB                           *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+let mongoose;
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.log(e);
+}
+const { assert } = require("chai");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const { Schema } = mongoose;
+const exerciseUserSchema = new Schema({
+  username: { type: String, required: true },
+});
+let ExerciseUser = mongoose.model("ExerciseUser", exerciseUserSchema);
+// TODO exercise model
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                     API routes                            *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+/**
+ * POST /api/users
+ *
+ * Creates a new user
+ * Request body (URL encoded):
+ * - username (String)
+ */
+app.post("/api/users", bodyParserUrlEncoded, (req, res) => {
+  const { username } = req.body;
 
+  // look up if a user with this name already exists ... if so res error
+  let promise = ExerciseUser.findOne({ username: username }).exec();
+  assert.ok(promise instanceof Promise);
+  promise.then((userObject) => {
+    if (userObject !== null) {
+      // The user exists, so return the object
+      res.json({
+        username: userObject.username,
+        _id: userObject._id,
+      });
+    } else {
+      // The user does not yet exist, so create a new one
+      const newUser = new ExerciseUser({ username: username });
+      newUser.save((err) => {
+        if (err) res.json({ Error: err });
+        else
+          res.json({
+            username: newUser.username,
+            _id: newUser._id,
+          });
+      });
+    }
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
